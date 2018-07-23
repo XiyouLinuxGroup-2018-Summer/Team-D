6954f9a271e9a1c0b9e731b4f0d6d9e21adea24c
Evil-crow <Evilcrow486@gmail.com>
2018-07-23 15:00:55 +0800

    统一目录至 刘甜/Vjudge/

---
+++ Code/刘甜/Vjudge/0/h.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+   int N,M;
+   int i,j,a,sum;
+   scanf("%d",&N);
+   for(j=0;j<N;j++)
+   {
+       scanf("%d",&M);
+       sum=0;
+       for(i=0;i<M;i++)
+       {
+          scanf("%d",&a);
+          sum+=a;
+       }
+       printf("%d\n",sum);
+       if(j<N-1)
+           printf("\n");
+   }
+}
